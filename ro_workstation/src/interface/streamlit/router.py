633bc0b5d6a0e58f02e045ad955fd79dc01c82f8
SATISH PANDIAN-Manager-RO-DINDIGUL <63039@iob.in>
2026-05-17 20:33:35 +0530

    Remove redundant Surveys & Feedback page and service

--- ro_workstation/src/interface/streamlit/router.py
+++ ro_workstation/src/interface/streamlit/router.py
@@ -19,7 +19,6 @@ PAGE_REGISTRY = {
     "Policy & Product Archive": "src.interface.streamlit.pages.knowledge",
     "Field Guardian": "src.interface.streamlit.pages.guardian",
     "Branch Visits": "src.interface.streamlit.pages.visits",
-    "Surveys & Feedback": "src.interface.streamlit.pages.surveys",
     "Admin": "src.interface.streamlit.pages.admin",
     "Letter Generator": "src.interface.streamlit.pages.letter_generator",
     "Branch Portal": "src.interface.streamlit.pages.branch_portal",
